71687635c7570f85c72c6c2b000f6b8307e7f6fd
Михаил Лукавский <lukavskym@ya.ru>
2024-08-09 19:04:54 +0300

    Рефакторинг

--- stage_3/test_fixture7.py
+++ stage_3/test_fixture7.py
@@ -11,7 +11,7 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('language', ["ru", "en-gb"])
+@pytest.mark.parametrize("language", ["ru", "en-gb"])
 def test_guest_should_see_login_link(browser, language):
     link = f"http://selenium1py.pythonanywhere.com/{language}/"
     browser.get(link)
